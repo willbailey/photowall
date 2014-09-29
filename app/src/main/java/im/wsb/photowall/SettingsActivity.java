9a8ed6b4bf1c9198187f193c14d20bddfb721c40
Will Bailey <wbailey@fb.com>
2014-09-28 21:46:05 -0700

    basic settings ui

--- app/src/main/java/im/wsb/photowall/SettingsActivity.java
+++ app/src/main/java/im/wsb/photowall/SettingsActivity.java
@@ -2,6 +2,7 @@ package im.wsb.photowall;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -14,17 +15,27 @@ import com.facebook.UiLifecycleHelper;
 
 import rx.Subscription;
 import rx.android.observables.AndroidObservable;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class SettingsActivity extends Activity {
   private Button mFacebookConnectButton;
   private UiLifecycleHelper mFacebookUiLifecycleHelper;
   private SharedPreferences mSharedPreferences;
   private Subscription mFbSessionSubscription;
-  private SeekBar mFlipIntervalSeek;
-  private SeekBar mNumberOfColumnSeek;
   private Subscription mNumberOfColumnsSubscription;
   private Subscription mFlipIntervalSubscription;
+  private Button mInterval1;
+  private Button mInterval2;
+  private Button mInterval3;
+  private Button mInterval4;
+  private View.OnClickListener mOnIntervalClick;
+  private Button mCols1;
+  private Button mCols2;
+  private Button mCols3;
+  private Button mCols4;
+  private View.OnClickListener mOnColsClick;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +50,46 @@ public class SettingsActivity extends Activity {
     setContentView(R.layout.settings);
 
     mFacebookConnectButton = (Button) findViewById(R.id.facebook_connect_button);
-    mFlipIntervalSeek = (SeekBar) findViewById(R.id.flip_interval_seek);
-    mNumberOfColumnSeek = (SeekBar) findViewById(R.id.number_of_columns_seek);
+    mOnIntervalClick = new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        setSelectedInterval(v);
+        String text = (String) ((Button) v).getText();
+        String formatted = text.replace("s", "");
+        float value = Float.valueOf(formatted);
+        PhotoWallApplication.get().setFlipInterval(value);
+      }
+    };
 
+    mInterval1 = (Button) findViewById(R.id.interval_1);
+    mInterval2 = (Button) findViewById(R.id.interval_2);
+    mInterval3 = (Button) findViewById(R.id.interval_3);
+    mInterval4 = (Button) findViewById(R.id.interval_4);
+
+    mInterval1.setOnClickListener(mOnIntervalClick);
+    mInterval2.setOnClickListener(mOnIntervalClick);
+    mInterval3.setOnClickListener(mOnIntervalClick);
+    mInterval4.setOnClickListener(mOnIntervalClick);
+
+    mOnColsClick = new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        setSelectedCols(v);
+        String text = (String) ((Button) v).getText();
+        int value = Integer.valueOf(text);
+        PhotoWallApplication.get().setNumberOfColumns(value);
+      }
+    };
+
+    mCols1 = (Button) findViewById(R.id.cols_1);
+    mCols2 = (Button) findViewById(R.id.cols_2);
+    mCols3 = (Button) findViewById(R.id.cols_3);
+    mCols4 = (Button) findViewById(R.id.cols_4);
+
+    mCols1.setOnClickListener(mOnColsClick);
+    mCols2.setOnClickListener(mOnColsClick);
+    mCols3.setOnClickListener(mOnColsClick);
+    mCols4.setOnClickListener(mOnColsClick);
 
     mFacebookConnectButton.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -49,40 +97,22 @@ public class SettingsActivity extends Activity {
         connectFacebook();
       }
     });
+  }
 
-    mFlipIntervalSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-      @Override
-      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-      }
+  private void setSelectedInterval(View v) {
+    mInterval1.setSelected(false);
+    mInterval2.setSelected(false);
+    mInterval3.setSelected(false);
+    mInterval4.setSelected(false);
+    v.setSelected(true);
+  }
 
-      @Override
-      public void onStartTrackingTouch(SeekBar seekBar) {
-
-      }
-
-      @Override
-      public void onStopTrackingTouch(SeekBar seekBar) {
-        float interval = mapValueInRange(seekBar.getProgress(), 0, 100, 0, 10);
-        PhotoWallApplication.get().setFlipInterval(interval);
-      }
-    });
-
-    mNumberOfColumnSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-      @Override
-      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-      }
-
-      @Override
-      public void onStartTrackingTouch(SeekBar seekBar) {
-
-      }
-
-      @Override
-      public void onStopTrackingTouch(SeekBar seekBar) {
-        int columns = (int) mapValueInRange(seekBar.getProgress(), 0, 100, 3, 30);
-        PhotoWallApplication.get().setNumberOfColumns(columns);
-      }
-    });
+  private void setSelectedCols(View v) {
+    mCols1.setSelected(false);
+    mCols2.setSelected(false);
+    mCols3.setSelected(false);
+    mCols4.setSelected(false);
+    v.setSelected(true);
   }
 
   private void connectFacebook() {
@@ -96,36 +126,60 @@ public class SettingsActivity extends Activity {
     mFacebookUiLifecycleHelper.onResume();
     mFbSessionSubscription =
         AndroidObservable.bindActivity(this, PhotoWallApplication.get().getFacebookSession())
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe(new Action1<Session>() {
               @Override
               public void call(Session session) {
                 if (session == null || !session.isOpened()) {
                   mFacebookConnectButton.setEnabled(true);
-                  mFacebookConnectButton.setText("Connect to Facebook");
+                  mFacebookConnectButton.setText("Connect Facebook");
                 } else {
                   mFacebookConnectButton.setEnabled(false);
-                  mFacebookConnectButton.setText("Facebook is Connected");
+                  mFacebookConnectButton.setText("Facebook Connected");
                 }
               }
             });
 
     mNumberOfColumnsSubscription =
         AndroidObservable.bindActivity(this, PhotoWallApplication.get().getNumberOfColumns())
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe(new Action1<Integer>() {
               @Override
               public void call(Integer columns) {
-                int columnsSeekPos = (int) mapValueInRange(columns, 3, 30, 0, 100);
-                mNumberOfColumnSeek.setProgress(columnsSeekPos);
+                switch (columns) {
+                  case 5:
+                    setSelectedCols(mCols1);
+                    break;
+                  case 8:
+                    setSelectedCols(mCols2);
+                    break;
+                  case 11:
+                    setSelectedCols(mCols3);
+                    break;
+                  case 30:
+                    setSelectedCols(mCols4);
+                    break;
+                }
               }
             });
 
     mFlipIntervalSubscription =
         AndroidObservable.bindActivity(this, PhotoWallApplication.get().getFlipInterval())
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe(new Action1<Float>() {
               @Override
               public void call(Float interval) {
-                int intervalSeekPos = (int) mapValueInRange(interval, 0, 10, 0, 100);
-                mFlipIntervalSeek.setProgress(intervalSeekPos);
+                if (interval == 0.5f) {
+                  setSelectedInterval(mInterval1);
+                } else if (interval == 3f) {
+                  setSelectedInterval(mInterval2);
+                } else if (interval == 10f) {
+                  setSelectedInterval(mInterval3);
+                } else if (interval == 30f) {
+                  setSelectedInterval(mInterval4);
+                }
               }
             });
 
